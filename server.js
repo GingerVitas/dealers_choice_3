0bd8d733641da27abe006a2cadb44d21d43c3fb7
Scott Johnson <singinbaritone@gmail.com>
2022-02-08 11:00:06 -0500

    forgot try/catch in second section

--- server.js
+++ server.js
@@ -88,59 +88,62 @@ catch(e) {
 });
 
 app.get('/quotes/:id', async (req, res, next) => {
-    const data = await client.query(`
-    SELECT reviews.id, companies.name AS company, production.name AS production, content, publication.name AS publication, author.name AS author
-    FROM reviews
-    JOIN production ON reviews.production_id = production.id
-    JOIN companies ON production.company_id = companies.id
-    JOIN publication ON reviews.publication_id = publication.id
-    JOIN author ON reviews.author_id = author.id
-    WHERE reviews.id=$1
-    ;`, [req.params.id]);
-    const quote = data.rows[0]
-    const fullQuote = `
-    <p>
-        <h2><span class='quoteHeader'>${quote.company} - ${quote.production}</h2>
-        <blockquote>${quote.content}</blockquote>
-        <small>${quote.author}, ${quote.publication}</small>
-    </p>
-    `
-    const html = `
-    <html>
-        <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Bridgette Gan, Soprano</title>
-        <link rel='stylesheet' href='/main.css'>
-        <link href="https://fonts.googleapis.com/css2?family=Carattere&family=Playfair+Display&display=swap" rel="stylesheet">
-        </head>
-        <body>
-            <header><a href='/'>Bridgette Gan, Soprano</a></header>
-            <section>
-                <div>
-                    <div id='nav'><nav>
-                        <ul>
-                            <li><a href='/home'>Home</a></li>
-                            <li><a href='/photos'>Photos</a></li>
-                            <li><a href='/press'>Press</a></li>
-                            <li><a href='contact.html'>Contact</a></li>
-                        </ul>
-                    </nav></div>
-                </div>
-                <div class='fullQuote'>
-                ${fullQuote}
-                </div>
-            </section>
-            <footer>
-            <p><span>
-                <a href='https://www.instagram.com/bridgettegan/'><img src='./public/images/Instagram Logo.png'></a> 
-                <a href='https://www.youtube.com/channel/UCqM6DgQrXauz5nmylU0iTyQ'><img src='./public/images/Youtube Logo.png' height='30px'></a>
-            </span></p>
-            <p>&copy 2022 Bridgette Gan</p>
-            </footer>
-        </body>   
-    </html>
-    `
-    res.send(html);
+    try {
+        const data = await client.query(`
+        SELECT reviews.id, companies.name AS company, production.name AS production, content, publication.name AS publication, author.name AS author
+        FROM reviews
+        JOIN production ON reviews.production_id = production.id
+        JOIN companies ON production.company_id = companies.id
+        JOIN publication ON reviews.publication_id = publication.id
+        JOIN author ON reviews.author_id = author.id
+        WHERE reviews.id=$1
+        ;`, [req.params.id]);
+        const quote = data.rows[0]
+        const fullQuote = `
+        <p>
+            <h2><span class='quoteHeader'>${quote.company} - ${quote.production}</h2>
+            <blockquote>${quote.content}</blockquote>
+            <small>${quote.author}, ${quote.publication}</small>
+        </p>
+        `
+        const html = `
+        <html>
+            <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Bridgette Gan, Soprano</title>
+            <link rel='stylesheet' href='/main.css'>
+            <link href="https://fonts.googleapis.com/css2?family=Carattere&family=Playfair+Display&display=swap" rel="stylesheet">
+            </head>
+            <body>
+                <header><a href='/'>Bridgette Gan, Soprano</a></header>
+                <section>
+                    <div>
+                        <div id='nav'><nav>
+                            <ul>
+                                <li><a href='/home'>Home</a></li>
+                                <li><a href='/photos'>Photos</a></li>
+                                <li><a href='/press'>Press</a></li>
+                                <li><a href='contact.html'>Contact</a></li>
+                            </ul>
+                        </nav></div>
+                    </div>
+                    <div class='fullQuote'>
+                    ${fullQuote}
+                    </div>
+                </section>
+                <footer>
+                <p><span>
+                    <a href='https://www.instagram.com/bridgettegan/'><img src='./public/images/Instagram Logo.png'></a> 
+                    <a href='https://www.youtube.com/channel/UCqM6DgQrXauz5nmylU0iTyQ'><img src='./public/images/Youtube Logo.png' height='30px'></a>
+                </span></p>
+                <p>&copy 2022 Bridgette Gan</p>
+                </footer>
+            </body>   
+        </html>
+        `
+        res.send(html);
+    }
+    catch(e) {next(e)}
 })
