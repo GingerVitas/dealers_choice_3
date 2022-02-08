98ae9cdafb6cc4e4d06ed523ed87670c151ed92a
Scott Johnson <singinbaritone@gmail.com>
2022-02-08 10:11:20 -0500

    Full page except contact

--- server.js
+++ server.js
@@ -1,20 +1,44 @@
-const { application } = require('express');
 const express = require('express');
-const app = express();
 const volleyball = require('volleyball');
-const client = require('/db.js');
+const client = require('./db.js');
+const app = express();
+app.use(express(volleyball));
+app.use(express.static(__dirname + '/public'));
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}.`)
 })
 
-app.get('/', async (req, res, next) => {
-    const list = await client.query();
+app.get('/home', (req, res, next) => {
+    try {
+        res.sendFile('public/index.html', {root: __dirname});
+    }
+    catch(e) {next(e)}
+})
+
+app.get('/photos', (req, res, next) => {
+    try {
+        res.sendFile('public/photos.html', {root:__dirname});
+    }
+    catch(e) {next(e)}
+})
+
+app.get('/press', async (req, res, next) => {
+   try {
+    const data = await client.query(`
+    SELECT reviews.id, companies.name AS company, production.name AS production, preview, publication.name AS publication, author.name AS author
+    FROM reviews
+    JOIN production ON reviews.production_id = production.id
+    JOIN companies ON production.company_id = companies.id
+    JOIN publication ON reviews.publication_id = publication.id
+    JOIN author ON reviews.author_id = author.id
+    ;`);
+    const list = data.rows;
     const presenterProdList = list.map(post => `
         <div class='pressQuotes'>
         <p>
-            <h2><span class='quoteHeader'><a href='/quotes/${post.id}'>${post.presenter} - ${post.production}</a></h2> <blockquote><a href='/quotes/${post.id}'>${post.preview}</a></blockquote> <small>${post.author}, ${post.publication}</small></span>
+            <h2><span class='quoteHeader'><a href='/quotes/${post.id}'>${post.company} - ${post.production}</a></h2> <blockquote><a href='/quotes/${post.id}'>${post.preview}</a></blockquote> <small>${post.author}, ${post.publication}</small></span>
         </p>
     </div>
     `
@@ -26,7 +50,7 @@ app.get('/', async (req, res, next) => {
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Bridgette Gan, Soprano - Press</title>
-        <link rel='stylesheet' href='./press.css'>
+        <link rel='stylesheet' href='./main.css'>
         <link href="https://fonts.googleapis.com/css2?family=Carattere&family=Playfair+Display&display=swap" rel="stylesheet">
         </head>
         <body>
@@ -36,9 +60,9 @@ app.get('/', async (req, res, next) => {
                     <h1>Singer | Actress | Educator</h1>
                     <div id='nav'><nav>
                         <ul>
-                            <li><a href='index.html'>Home</a></li>
-                            <li><a href='photos.html'>Photos</a></li>
-                            <li><a href='press.html'>Press</a></li>
+                            <li><a href='/home'>Home</a></li>
+                            <li><a href='/photos'>Photos</a></li>
+                            <li><a href='/press'>Press</a></li>
                             <li><a href='contact.html'>Contact</a></li>
                         </ul>
                     </nav></div>
@@ -47,9 +71,78 @@ app.get('/', async (req, res, next) => {
                 ${presenterProdList}
                 </div>
             </section>
+            <footer>
+            <p><span>
+                <a href='https://www.instagram.com/bridgettegan/'><img src='images/Instagram Logo.png'></a> 
+                <a href='https://www.youtube.com/channel/UCqM6DgQrXauz5nmylU0iTyQ'><img src='images/Youtube Logo.png' height='30px'></a>
+            </span></p>
+            <p>&copy 2022 Bridgette Gan</p>
+            </footer>
         </body>   
     </html>
     `;
     res.send(html)
-})
+}
+catch(e) { 
+    next(e)
+}
+});
 
+app.get('/quotes/:id', async (req, res, next) => {
+    const data = await client.query(`
+    SELECT reviews.id, companies.name AS company, production.name AS production, content, publication.name AS publication, author.name AS author
+    FROM reviews
+    JOIN production ON reviews.production_id = production.id
+    JOIN companies ON production.company_id = companies.id
+    JOIN publication ON reviews.publication_id = publication.id
+    JOIN author ON reviews.author_id = author.id
+    WHERE reviews.id=$1
+    ;`, [req.params.id]);
+    const quote = data.rows[0]
+    const fullQuote = `
+    <p>
+        <h2><span class='quoteHeader'>${quote.company} - ${quote.production}</h2>
+        <blockquote>${quote.content}</blockquote>
+        <small>${quote.author}, ${quote.publication}</small>
+    </p>
+    `
+    const html = `
+    <html>
+        <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Bridgette Gan, Soprano</title>
+        <link rel='stylesheet' href='/main.css'>
+        <link href="https://fonts.googleapis.com/css2?family=Carattere&family=Playfair+Display&display=swap" rel="stylesheet">
+        </head>
+        <body>
+            <header><a href='/'>Bridgette Gan, Soprano</a></header>
+            <section>
+                <div>
+                    <h1>Singer | Actress | Educator</h1>
+                    <div id='nav'><nav>
+                        <ul>
+                            <li><a href='/home'>Home</a></li>
+                            <li><a href='/photos'>Photos</a></li>
+                            <li><a href='/press'>Press</a></li>
+                            <li><a href='contact.html'>Contact</a></li>
+                        </ul>
+                    </nav></div>
+                </div>
+                <div class='fullQuote'>
+                ${fullQuote}
+                </div>
+            </section>
+            <footer>
+            <p><span>
+                <a href='https://www.instagram.com/bridgettegan/'><img src='./public/images/Instagram Logo.png'></a> 
+                <a href='https://www.youtube.com/channel/UCqM6DgQrXauz5nmylU0iTyQ'><img src='./public/images/Youtube Logo.png' height='30px'></a>
+            </span></p>
+            <p>&copy 2022 Bridgette Gan</p>
+            </footer>
+        </body>   
+    </html>
+    `
+    res.send(html);
+})
