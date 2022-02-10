f7c26caa8673c50f8301324509f55e124ea270e3
Scott Johnson <singinbaritone@gmail.com>
2022-02-10 13:48:26 -0500

    working routes

---
+++ routes/routes.js
@@ -0,0 +1,153 @@
+const client = require('../db')
+const express = require('express');
+const router = express.Router();
+const path = require('path')
+
+
+router.get('/', (req, res, next) => {
+    try {
+        res.sendFile(path.join(__dirname, '..', 'public', 'index.html',));
+    }
+    catch(e) {next(e)}
+});
+
+router.get('/photos', (req, res, next) => {
+    try {
+        res.sendFile(path.join(__dirname, '..', 'public', 'photos.html',));
+    }
+    catch(e) {next(e)}
+});
+
+router.get('/contact', (req, res, next) => {
+    try {
+        res.sendFile(path.join(__dirname, '..', 'public', 'contact.html',));
+    }
+    catch(e) {next(e)}
+});
+
+router.get('/press', async (req, res, next) => {
+    try {
+     const data = await client.query(`
+     SELECT reviews.id, companies.name AS company, production.name AS production, preview, publication.name AS publication, author.name AS author
+     FROM reviews
+     JOIN production ON reviews.production_id = production.id
+     JOIN companies ON production.company_id = companies.id
+     JOIN publication ON reviews.publication_id = publication.id
+     JOIN author ON reviews.author_id = author.id
+     ;`);
+     const list = data.rows;
+     const presenterProdList = list.map(post => `
+         <div class='pressQuotes'>
+         <p>
+             <h2><span class='quoteHeader'><a href='/quotes/${post.id}'>${post.company} - ${post.production}</a></h2> <blockquote><a href='/quotes/${post.id}'>${post.preview}</a></blockquote> <small>${post.author}, ${post.publication}</small></span>
+         </p>
+     </div>
+     `
+     ).join('')
+     const html = `
+     <html>
+         <head>
+         <meta charset="UTF-8">
+         <meta http-equiv="X-UA-Compatible" content="IE=edge">
+         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+         <title>Bridgette Gan, Soprano - Press</title>
+         <link rel='stylesheet' href='./main.css'>
+         <link href="https://fonts.googleapis.com/css2?family=Carattere&family=Playfair+Display&display=swap" rel="stylesheet">
+         </head>
+         <body>
+             <header>Bridgette Gan, Soprano</header>
+             <section>
+                 <div>
+                 <h1>Singer | Actress | Educator</h1>
+                     <div id='nav'><nav>
+                         <ul>
+                             <li><a href='/'>Home</a></li>
+                             <li><a href='/photos'>Photos</a></li>
+                             <li><a href='/press'>Press</a></li>
+                             <li><a href='contact.html'>Contact</a></li>
+                         </ul>
+                     </nav></div>
+                 </div>
+                 <div>
+                 ${presenterProdList}
+                 </div>
+             </section>
+             <footer>
+             <p><span>
+                 <a href='https://www.instagram.com/bridgettegan/'><img src='/images/Instagram Logo.png'></a> 
+                 <a href='https://www.youtube.com/channel/UCqM6DgQrXauz5nmylU0iTyQ'><img src='/images/Youtube Logo.png' height='30px'></a>
+             </span></p>
+             <p>&copy 2022 Bridgette Gan</p>
+             </footer>
+         </body>   
+     </html>
+     `;
+     res.send(html)
+ }
+ catch(e) { 
+     next(e)
+ }
+ });
+ 
+ router.get('/quotes/:id', async (req, res, next) => {
+     try {
+         const data = await client.query(`
+         SELECT reviews.id, companies.name AS company, production.name AS production, content, publication.name AS publication, author.name AS author
+         FROM reviews
+         JOIN production ON reviews.production_id = production.id
+         JOIN companies ON production.company_id = companies.id
+         JOIN publication ON reviews.publication_id = publication.id
+         JOIN author ON reviews.author_id = author.id
+         WHERE reviews.id=$1
+         ;`, [req.params.id]);
+         const quote = data.rows[0]
+         const fullQuote = `
+         <p>
+             <h2><span class='quoteHeader'><a href='/press'>${quote.company} - ${quote.production}</a></h2>
+             <blockquote>${quote.content}</blockquote>
+             <small>${quote.author}, ${quote.publication}</small>
+         </p>
+         `
+         const html = `
+         <html>
+             <head>
+             <meta charset="UTF-8">
+             <meta http-equiv="X-UA-Compatible" content="IE=edge">
+             <meta name="viewport" content="width=device-width, initial-scale=1.0">
+             <title>Bridgette Gan, Soprano</title>
+             <link rel='stylesheet' href='/main.css'>
+             <link href="https://fonts.googleapis.com/css2?family=Carattere&family=Playfair+Display&display=swap" rel="stylesheet">
+             </head>
+             <body>
+                 <header><a href='/'>Bridgette Gan, Soprano</a></header>
+                 <section>
+                     <div>
+                         <div id='nav'><nav>
+                             <ul>
+                                 <li><a href='/'>Home</a></li>
+                                 <li><a href='/photos'>Photos</a></li>
+                                 <li><a href='/press'>Press</a></li>
+                                 <li><a href='contact.html'>Contact</a></li>
+                             </ul>
+                         </nav></div>
+                     </div>
+                     <div class='fullQuote'>
+                     ${fullQuote}
+                     </div>
+                 </section>
+                 <footer>
+                 <p><span>
+                     <a href='https://www.instagram.com/bridgettegan/'><img src='/images/Instagram Logo.png'></a> 
+                     <a href='https://www.youtube.com/channel/UCqM6DgQrXauz5nmylU0iTyQ'><img src='/images/Youtube Logo.png' height='30px'></a>
+                 </span></p>
+                 <p>&copy 2022 Bridgette Gan</p>
+                 </footer>
+             </body>   
+         </html>
+         `
+         res.send(html);
+     }
+     catch(e) {next(e)}
+ })
+ 
+ module.exports = router;
